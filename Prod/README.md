64f8e0be099688f89643587eea160c9b810aeea5
dorlanpabon <camilopa1998@hotmail.com>
2022-05-28 22:25:35 -0500

    Device 27'

---
+++ Prod/README.md
@@ -0,0 +1,2 @@
+# Extensión de utilidades AWS
+Extensión para funcionalidades de AWS, se hizo uso de los iframes internos que se manejan dentro de una pagina web.
--- assets/js/content.js
+++ assets/js/content.js
@@ -1,8 +1,10 @@
 const awsuiIconURL = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" focusable="false" aria-hidden="true">	<path d="M16.469,8.924l-2.414,2.413c-0.156,0.156-0.408,0.156-0.564,0c-0.156-0.155-0.156-0.408,0-0.563l2.414-2.414c1.175-1.175,1.175-3.087,0-4.262c-0.57-0.569-1.326-0.883-2.132-0.883s-1.562,0.313-2.132,0.883L9.227,6.511c-1.175,1.175-1.175,3.087,0,4.263c0.288,0.288,0.624,0.511,0.997,0.662c0.204,0.083,0.303,0.315,0.22,0.52c-0.171,0.422-0.643,0.17-0.52,0.22c-0.473-0.191-0.898-0.474-1.262-0.838c-1.487-1.485-1.487-3.904,0-5.391l2.414-2.413c0.72-0.72,1.678-1.117,2.696-1.117s1.976,0.396,2.696,1.117C17.955,5.02,17.955,7.438,16.469,8.924 M10.076,7.825c-0.205-0.083-0.437,0.016-0.52,0.22c-0.083,0.205,0.016,0.437,0.22,0.52c0.374,0.151,0.709,0.374,0.997,0.662c1.176,1.176,1.176,3.088,0,4.263l-2.414,2.413c-0.569,0.569-1.326,0.883-2.131,0.883s-1.562-0.313-2.132-0.883c-1.175-1.175-1.175-3.087,0-4.262L6.51,9.227c0.156-0.155,0.156-0.408,0-0.564c-0.156-0.156-0.408-0.156-0.564,0l-2.414,2.414c-1.487,1.485-1.487,3.904,0,5.391c0.72,0.72,1.678,1.116,2.696,1.116s1.976-0.396,2.696-1.116l2.414-2.413c1.487-1.486,1.487-3.905,0-5.392C10.974,8.298,10.55,8.017,10.076,7.825"></path></svg>`;
+const awsuiIconJSON = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 58 58" style="enable-background:new 0 0 58 58;" xml:space="preserve"> <g> <path d="M50.949,12.187l-1.361-1.361l-9.504-9.505c-0.001-0.001-0.001-0.001-0.002-0.001l-0.77-0.771 C38.957,0.195,38.486,0,37.985,0H8.963C7.776,0,6.5,0.916,6.5,2.926V39v16.537V56c0,0.837,0.841,1.652,1.836,1.909 c0.051,0.014,0.1,0.033,0.152,0.043C8.644,57.983,8.803,58,8.963,58h40.074c0.16,0,0.319-0.017,0.475-0.048 c0.052-0.01,0.101-0.029,0.152-0.043C50.659,57.652,51.5,56.837,51.5,56v-0.463V39V13.978C51.5,13.211,51.407,12.644,50.949,12.187 z M39.5,3.565L47.935,12H39.5V3.565z M8.963,56c-0.071,0-0.135-0.025-0.198-0.049C8.61,55.877,8.5,55.721,8.5,55.537V41h41v14.537 c0,0.184-0.11,0.34-0.265,0.414C49.172,55.975,49.108,56,49.037,56H8.963z M8.5,39V2.926C8.5,2.709,8.533,2,8.963,2h28.595 C37.525,2.126,37.5,2.256,37.5,2.391V13.78c-0.532-0.48-1.229-0.78-2-0.78c-0.553,0-1,0.448-1,1s0.447,1,1,1c0.552,0,1,0.449,1,1v4 c0,1.2,0.542,2.266,1.382,3c-0.84,0.734-1.382,1.8-1.382,3v4c0,0.551-0.448,1-1,1c-0.553,0-1,0.448-1,1s0.447,1,1,1 c1.654,0,3-1.346,3-3v-4c0-1.103,0.897-2,2-2c0.553,0,1-0.448,1-1s-0.447-1-1-1c-1.103,0-2-0.897-2-2v-4 c0-0.771-0.301-1.468-0.78-2h11.389c0.135,0,0.265-0.025,0.391-0.058c0,0.015,0.001,0.021,0.001,0.036V39H8.5z"/> <path d="M16.354,51.43c-0.019,0.446-0.171,0.764-0.458,0.95s-0.672,0.28-1.155,0.28c-0.191,0-0.396-0.022-0.615-0.068 s-0.429-0.098-0.629-0.157s-0.385-0.123-0.554-0.191s-0.299-0.135-0.39-0.198l-0.697,1.107c0.183,0.137,0.405,0.26,0.67,0.369 s0.54,0.207,0.827,0.294s0.565,0.15,0.834,0.191s0.504,0.062,0.704,0.062c0.401,0,0.791-0.039,1.169-0.116 c0.378-0.077,0.713-0.214,1.005-0.41s0.524-0.456,0.697-0.779s0.26-0.723,0.26-1.196v-7.848h-1.668V51.43z"/> <path d="M25.083,49.064c-0.314-0.228-0.654-0.422-1.019-0.581s-0.702-0.323-1.012-0.492s-0.569-0.364-0.779-0.588 s-0.314-0.518-0.314-0.882c0-0.146,0.036-0.299,0.109-0.458s0.173-0.303,0.301-0.431s0.273-0.234,0.438-0.321 s0.337-0.139,0.52-0.157c0.328-0.027,0.597-0.032,0.807-0.014s0.378,0.05,0.506,0.096s0.226,0.091,0.294,0.137 s0.13,0.082,0.185,0.109c0.009-0.009,0.036-0.055,0.082-0.137s0.101-0.185,0.164-0.308s0.132-0.255,0.205-0.396 s0.137-0.271,0.191-0.39c-0.265-0.173-0.61-0.299-1.039-0.376s-0.853-0.116-1.271-0.116c-0.41,0-0.8,0.063-1.169,0.191 s-0.692,0.313-0.971,0.554s-0.499,0.535-0.663,0.882S20.4,46.13,20.4,46.576c0,0.492,0.104,0.902,0.314,1.23 s0.474,0.613,0.793,0.854s0.661,0.451,1.025,0.629s0.704,0.355,1.019,0.533s0.576,0.376,0.786,0.595s0.314,0.483,0.314,0.793 c0,0.511-0.148,0.896-0.444,1.155s-0.723,0.39-1.278,0.39c-0.183,0-0.378-0.019-0.588-0.055s-0.419-0.084-0.629-0.144 s-0.412-0.123-0.608-0.191s-0.357-0.139-0.485-0.212l-0.287,1.176c0.155,0.137,0.34,0.253,0.554,0.349s0.439,0.171,0.677,0.226 c0.237,0.055,0.472,0.094,0.704,0.116s0.458,0.034,0.677,0.034c0.511,0,0.966-0.077,1.367-0.232s0.738-0.362,1.012-0.622 s0.485-0.561,0.636-0.902s0.226-0.695,0.226-1.06c0-0.538-0.104-0.978-0.314-1.319S25.397,49.292,25.083,49.064z"/> <path d="M34.872,45.072c-0.378-0.429-0.82-0.754-1.326-0.978s-1.06-0.335-1.661-0.335s-1.155,0.111-1.661,0.335 s-0.948,0.549-1.326,0.978s-0.675,0.964-0.889,1.606s-0.321,1.388-0.321,2.235s0.107,1.595,0.321,2.242s0.511,1.185,0.889,1.613 s0.82,0.752,1.326,0.971s1.06,0.328,1.661,0.328s1.155-0.109,1.661-0.328s0.948-0.542,1.326-0.971s0.675-0.966,0.889-1.613 s0.321-1.395,0.321-2.242s-0.107-1.593-0.321-2.235S35.25,45.501,34.872,45.072z M34.195,50.698 c-0.137,0.487-0.326,0.882-0.567,1.183s-0.515,0.518-0.82,0.649s-0.627,0.198-0.964,0.198c-0.328,0-0.641-0.07-0.937-0.212 s-0.561-0.364-0.793-0.67s-0.415-0.699-0.547-1.183s-0.203-1.066-0.212-1.75c0.009-0.702,0.082-1.294,0.219-1.777 c0.137-0.483,0.326-0.877,0.567-1.183s0.515-0.521,0.82-0.649s0.627-0.191,0.964-0.191c0.328,0,0.641,0.068,0.937,0.205 s0.561,0.36,0.793,0.67s0.415,0.704,0.547,1.183s0.203,1.06,0.212,1.743C34.405,49.616,34.332,50.211,34.195,50.698z"/> <polygon points="44.012,50.869 40.061,43.924 38.393,43.924 38.393,54 40.061,54 40.061,47.055 44.012,54 45.68,54 45.68,43.924 44.012,43.924 "/> <path d="M20.5,20v-4c0-0.551,0.448-1,1-1c0.553,0,1-0.448,1-1s-0.447-1-1-1c-1.654,0-3,1.346-3,3v4c0,1.103-0.897,2-2,2 c-0.553,0-1,0.448-1,1s0.447,1,1,1c1.103,0,2,0.897,2,2v4c0,1.654,1.346,3,3,3c0.553,0,1-0.448,1-1s-0.447-1-1-1 c-0.552,0-1-0.449-1-1v-4c0-1.2-0.542-2.266-1.382-3C19.958,22.266,20.5,21.2,20.5,20z"/> <circle cx="28.5" cy="19.5" r="1.5"/> <path d="M28.5,25c-0.553,0-1,0.448-1,1v3c0,0.552,0.447,1,1,1s1-0.448,1-1v-3C29.5,25.448,29.053,25,28.5,25z"/> </g> </svg>`;
 console.log('content.js loaded');
 $(function () {
 	console.log('content.js loaded');
 	var doc = document;
+	//Funcion para esperar un elemento del DOM
 	function waitForElm(doc, selector) {
 		return new Promise(resolve => {
 			if (doc.querySelector(selector)) {
@@ -37,10 +39,11 @@ $(function () {
 
 		console.log(iframeBody);
 
-		var div = document.createElement('div');
-		div.className = 'panel-menu-item is-not-selected';
-		div.id = 'geturls';
-		div.innerHTML = `
+		//boton para obtener las url
+		var geturls = document.createElement('div');
+		geturls.className = 'panel-menu-item is-not-selected';
+		geturls.id = 'geturls';
+		geturls.innerHTML = `
 			<awsui-icon initialized="true">
 				<span class="awsui-icon awsui-icon-size-normal awsui-icon-variant-normal">
 					${awsuiIconURL}
@@ -48,10 +51,119 @@ $(function () {
 			</awsui-icon>
 			<p>Get URL's</p>
 					`;
-		iframeDoc.querySelector("#logs-micro-console__root > div > div > div > div > div.panel-menu").appendChild(div);
-
+		//boton para ver los JSON
+		var getjsons = document.createElement('div');
+		getjsons.className = 'panel-menu-item is-not-selected';
+		getjsons.id = 'getjsons';
+		getjsons.innerHTML = `
+			<awsui-icon initialized="true">
+				<span class="awsui-icon awsui-icon-size-normal awsui-icon-variant-normal">
+					${awsuiIconJSON}
+				</span>
+			</awsui-icon>
+			<p>Get JSON's</p>
+					`;
+		//Modal para ver los JSON
+		var modal = document.createElement('div');
+		modal.className = 'modal';
+		modal.id = 'modal-json';
+		modal.style.display = 'none';
+		modal.innerHTML = `
+			<awsui-modal initialized="true">
+				<div class="awsui-modal-tabtrap" tabindex="0"></div>
+				<div class="awsui-modal-container awsui-modal-expandtofit " role="dialog" aria-modal="true"
+					aria-labelledby="awsui-modal-0" >
+					<div class="awsui-modal-dialog awsui-modal-expandtofit awsui-modal-size-max" tabindex="-1" style="max-width: calc(100vw - 3rem) !important;">
+						<div class="awsui-modal-content awsui-util-container">
+							<div class="awsui-modal-header awsui-util-container-header">
+								<div class="awsui-modal-title" id="awsui-modal-0">
+									<div awsui-modal-region="header"><span>JSON Viewer</span></div>
+								</div>
+								<awsui-button class="awsui-modal-dismiss-control closeModal" initialized="true"><button
+										class="awsui-button awsui-button-no-text awsui-button-variant-icon awsui-hover-child-icons closeModal"
+										type="button" override-focus="">
+										<awsui-icon class="awsui-button-icon awsui-button-icon-left" initialized="true"><span
+												class="awsui-icon awsui-icon-size-normal awsui-icon-variant-normal"><svg
+													xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" focusable="false"
+													aria-hidden="true">
+													<path d="M2 2l12 12M14 2L2 14"></path>
+												</svg></span></awsui-icon>
+									</button></awsui-button>
+							</div>
+							<div class="awsui-modal-body awsui-modal-expandtofit">
+								<div awsui-modal-region="content" id="contentJSON">
+								<div id="jsoneditor1"></div>
+								<div id="jsoneditor2"></div>
+									
+								</div>
+							</div>
+							<div class="awsui-modal-footer awsui-util-container-footer" awsui-modal-region="footer"><span><span
+										class="flex flex-end">
+										<awsui-button initialized="true" class="closeModal"><button
+												class="awsui-button awsui-button-variant-link awsui-hover-child-icons closeModal" type="submit"
+												override-focus="">
+												<span awsui-button-region="text" class="closeModal">
+													<span  class="closeModal">Cerrar</span>
+												</span>
+											</button>
+										</awsui-button>
+									</span></span>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="awsui-modal-overlay closeModal"></div>
+				<div class="awsui-modal-tabtrap closeModal" tabindex="0"></div>
+			</awsui-modal>
+			`;
+		//CSS para el editor de JSON
+		var linkjsoneditor = document.createElement('link');
+		linkjsoneditor.rel = 'stylesheet';
+		linkjsoneditor.href = 'https://cdnjs.cloudflare.com/ajax/libs/jsoneditor/9.7.4/jsoneditor.min.css';
+		//JS para el editor de JSON
+		var scriptjsoneditor = document.createElement('script');
+		scriptjsoneditor.src = 'https://cdnjs.cloudflare.com/ajax/libs/jsoneditor/9.7.4/jsoneditor.min.js';
+		//CSS para el editor de JSON
+		var stylejsoneditor = document.createElement('style');
+		stylejsoneditor.innerHTML = `
+								#jsoneditor1,
+								#jsoneditor2 {
+								width: 49%;
+								height: 500px;
+								margin-right: 10px;
+								display: inline-block;
+								}`;
+		//inscrución de los elementos en el DOM del iframe
+		iframeDoc.head.appendChild(stylejsoneditor);
+		iframeDoc.head.appendChild(linkjsoneditor);
+		iframeDoc.head.appendChild(linkjsoneditor);
+		iframeDoc.querySelector("#query-detail-page").appendChild(modal);
+		iframeDoc.querySelector("#logs-micro-console__root > div > div > div > div > div.panel-menu").appendChild(geturls);
+		iframeDoc.querySelector("#logs-micro-console__root > div > div > div > div > div.panel-menu").appendChild(getjsons);
+		//listen for clicks on the class closeModal
+		iframeDoc.querySelectorAll('.closeModal').forEach(r => {
+			r.addEventListener('click', function () { iframeDoc.querySelector('#modal-json').style.display = 'none'; })
+		});
+		// create the editor
+		const editor1 = new JSONEditor(iframeDoc.getElementById('jsoneditor1'), {
+			mode: 'code',
+			modes: ['code', 'form', 'text', 'tree', 'view', 'preview'], // allowed modes
+			onChangeText: function (jsonString) {
+				editor2.updateText(jsonString)
+			}
+		})
+		// create editor 2
+		const editor2 = new JSONEditor(iframeDoc.getElementById('jsoneditor2'), {
+			mode: 'tree',
+			modes: ['code', 'form', 'text', 'tree', 'view', 'preview'], // allowed modes
+			onChangeText: function (jsonString) {
+				editor1.updateText(jsonString)
+			}
+		})
+		//Funcion para obtener las URLs de los logs
 		function cargarURLS() {
 			console.log("cargarURLS");
+			//recorrermos todos los tr de los elmentos con desplegable
 			iframeDoc.querySelectorAll("div[class='logs-insights-expanded-row']>table>tbody>tr").forEach(r => {
 				var execution_arn
 				var urlLog
@@ -66,28 +178,79 @@ $(function () {
 				};
 				//regex to get the url lambda
 				if (RegExp('\d*:\/aws\/[a-zA-Z0-9-_]*\/[a-zA-Z0-9-_]*').test(tdValue) && tdTitle.includes("@log")) {
+					//remplazamos el / por $252F para que amazon lo reconozca
 					textLog = tdValue.replace(/\d*:/, '').replaceAll('/', '$252F');
 					urlLog = 'https://us-east-1.console.aws.amazon.com/cloudwatch/home?region=us-east-1#logsV2:log-groups/log-group/' + textLog;
 					console.log(tdValueHTML.innerHTML = '<a href="' + urlLog + '" target="_blank">' + tdValue + '</a>');
 					//recorrer todos los tdValueHTML para obtener el logStream
-					var tdValueParent
+					var tdValueParent;
 					var parenNode = r.parentNode.children.length;
+					var textLogStream;
 					for (var i = 0; i < parenNode; i++) {
 						if (r.parentNode.children[i].innerText.includes("@logStream")) {
 							tdValueParent = r.parentNode.children[i].children[1].innerText;
+							//remplazamos el / por $252F para que amazon lo reconozca
 							textLogStream = tdValueParent.replaceAll('/', '$252F');
 							logStream = 'https://us-east-1.console.aws.amazon.com/cloudwatch/home?region=us-east-1#logsV2:log-groups/log-group/' + textLog + '/log-events/' + textLogStream;
 							console.log(r.parentNode.children[i].children[1].innerHTML = '<a href="' + logStream + '" target="_blank">' + tdValueParent + '</a>');
+						}
+						//if @timestamp
+						if (r.parentNode.children[i].innerText.includes("@timestamp")) {
+							timestamp = r.parentNode.children[i].children[1].innerText;
+							urllogStreamTimestamp = 'https://us-east-1.console.aws.amazon.com/cloudwatch/home?region=us-east-1#logsV2:log-groups/log-group/' + textLog + '/log-events/' + textLogStream + '$3Fstart$3D' + timestamp + '$26end$3D' + (Number(timestamp) + 1);
+							console.log(r.parentNode.children[i].children[1].innerHTML = '<a href="' + urllogStreamTimestamp + '" target="_blank">' + timestamp + '</a>');
 						}
 					}
 				};
 			});
 		}
-
+		//Funcion para cargar los JSON en el editor
+		function cargarJsonView() {
+			iframeDoc.querySelectorAll("div[class='logs-insights-expanded-row']>table>tbody>tr").forEach(r => {
+				var tdTitle = r.children[0].innerText;
+				var tdValue = r.children[1].innerText;
+				if (tdTitle.includes("@message")) {
+					try {
+						var json
+						if (tdValue.includes("INFO EVENT: ") && tdValue.includes("{") && tdValue.includes("}") && tdValue.includes("\"")) {
+							json = JSON.parse(tdValue.split("INFO EVENT: ")[1]);
+						} else {
+							json = JSON.parse(tdValue);
+						}
+						r.addEventListener('click', () => {
+							editor1.set(json)
+							editor2.set(json)
+							editor2.expandAll()
+							iframeDoc.getElementById('modal-json').style.display = 'block';
+						});
+						r.children[1].style.background = '#a22727';
+						r.children[1].style.color = 'white';
+						r.children[1].style.cursor = 'pointer';
+					} catch (e) {
+						console.log('No es JSON');
+					}
+				}
+			});
+		}
+		//Escucha los clicks en el boton de cargar
 		iframeDoc.querySelector("#geturls").addEventListener("click", () => {
 			cargarURLS()
+		}, false);
+		//Escucha los clicks en el boton de cargar
+		iframeDoc.querySelector("#getjsons").addEventListener("click", () => {
+			cargarJsonView()
 		}, false);
 
 	});
 
 });
+/* 
+
+convert to json
+{  user: 'postgres',  password: 'YQQ2e4NLdUNs8WFb',  host: 'dev-wekall-cc-pg11.cluster-ro-cvdtlcbmntuu.us-east-1.rds.amazonaws.com',  database: 'customers',  port: '5432'}
+
+//funcion para covertir string sin comillas {  user: 'postgres',  password: 'YQQ2e4NLdUNs8WFb',  host: 'dev-wekall-cc-pg11.cluster-ro-cvdtlcbmntuu.us-east-1.rds.amazonaws.com',  database: 'customers',  port: '5432'} a json
+function convertToJson(str) {
+	var json = JSON.parse(str);
+	return json;
+} */
